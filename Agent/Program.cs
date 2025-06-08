f047a7ace16c342b7052357dea98f2ad0b5b3d57
coby98765 <coby98765@gmail.com>
2025-06-08 09:35:11 +0300

    Add project files.

---
+++ Agent/Program.cs
@@ -0,0 +1,14 @@
+﻿using agent.Models;
+using MySql.Data.MySqlClient;
+
+
+namespace agent.Models
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+
+            }
+        }
+    }
